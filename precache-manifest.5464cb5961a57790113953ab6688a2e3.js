0d593a9e0bc47307d363f08caa9cda5e15cd064b
Olusola Babajide-kassim <bab.andrex@gmail.com>
2022-05-24 22:03:01 -0700

    Updates

---
+++ precache-manifest.5464cb5961a57790113953ab6688a2e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c692781001e5d1deea99",
+    "url": "/to-do-list/static/css/main.39511fee.chunk.css"
+  },
+  {
+    "revision": "c692781001e5d1deea99",
+    "url": "/to-do-list/static/js/main.e457cbf5.chunk.js"
+  },
+  {
+    "revision": "c9cbb904c5304ec3ac58",
+    "url": "/to-do-list/static/js/runtime~main.7dba5ce5.js"
+  },
+  {
+    "revision": "ea41dc666d14110e4ead",
+    "url": "/to-do-list/static/css/2.55eb4657.chunk.css"
+  },
+  {
+    "revision": "ea41dc666d14110e4ead",
+    "url": "/to-do-list/static/js/2.e4851313.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/to-do-list/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "217e2301c98827f79e9e2cb3e64638e3",
+    "url": "/to-do-list/index.html"
+  }
+];
